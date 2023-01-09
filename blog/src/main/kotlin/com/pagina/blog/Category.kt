627be9534255bfe51474423f84626b97c172a445
josue.messias <jmessiasweb>
2023-01-09 13:55:15 -0300

    implementação das classeCategory/CategoryRepository/DataLoader/IndexController/LoginController/SignUpController

--- blog/src/main/kotlin/com/pagina/blog/Category.kt
+++ blog/src/main/kotlin/com/pagina/blog/Category.kt
@@ -1,3 +1,15 @@
 package com.pagina.blog
 
-data class Category()
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+
+@Entity
+data class Category(
+    @Id
+    @GeneratedValue
+    var id: Long = 0,
+    var name: String = ""
+
+
+)
--- blog/src/main/kotlin/com/pagina/blog/DataLoader.kt
+++ blog/src/main/kotlin/com/pagina/blog/DataLoader.kt
@@ -1,8 +1,52 @@
 package com.pagina.blog
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.boot.CommandLineRunner
 import org.springframework.context.annotation.Configuration
 
 
 @Configuration
-class DatabaseInitialization {
+class DataLoader(
+    private val userRepository: UserRepository,
+    private val categoryRepository: CategoryRepository
+) : CommandLineRunner {
+
+    private val logger: Logger = LoggerFactory.getLogger(javaClass)
+
+    override fun run(vararg args: String?) {
+        loadUser()
+
+        loadCategories()
+    }
+
+    private fun loadCategories() {
+        if (categoryRepository.count() == 0L) {
+            listOf(
+                Category(name = "Technology"),
+                Category(name = "World"),
+                Category(name = "U.S."),
+                Category(name = "Design"),
+                Category(name = "Culture"),
+                Category(name = "Business"),
+                Category(name = "Politics"),
+                Category(name = "Opinion"),
+                Category(name = "Science"),
+                Category(name = "Health"),
+                Category(name = "Style"),
+                Category(name = "Travel")
+            ).also { categoryRepository.saveAll(it) }
+        }
+    }
+
+    private fun loadUser() {
+        if (userRepository.count() == 0L) {
+            val user = User(
+                name = "Administrador",
+                email = "admin@blog.com",
+                password = "password"
+            )
+            userRepository.save(user).also { logger.info(it.toString()) }
+        }
+    }
 }
