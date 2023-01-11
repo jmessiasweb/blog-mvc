8313661130c3234f169b1802e1de5637fcbc57c2
josue.messias <jmessiasweb>
2023-01-11 14:15:22 -0300

    implementação das cÃlasses ArticleController/ ArticleRepository/ IndexController

--- blog/src/main/kotlin/com/pagina/blog/ArticleRepository.kt
+++ blog/src/main/kotlin/com/pagina/blog/ArticleRepository.kt
@@ -2,7 +2,9 @@ package com.pagina.blog
 
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.util.Optional
 
 @Repository
 interface ArticleRepository: JpaRepository<Article, Long> {
+    fun findByUserId(id: Long): Optional<Author>
 }
--- blog/src/main/kotlin/com/pagina/blog/DataLoader.kt
+++ blog/src/main/kotlin/com/pagina/blog/DataLoader.kt
@@ -4,20 +4,23 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.boot.CommandLineRunner
 import org.springframework.context.annotation.Configuration
+import java.time.LocalDateTime
 
 
 @Configuration
 class DataLoader(
     private val userRepository: UserRepository,
-    private val categoryRepository: CategoryRepository
+    private val categoryRepository: CategoryRepository,
+    private val articleRepository: ArticleRepository,
+    private val authorRepository: AuthorRepository
 ) : CommandLineRunner {
 
     private val logger: Logger = LoggerFactory.getLogger(javaClass)
 
     override fun run(vararg args: String?) {
         loadUser()
-
         loadCategories()
+        loadArticles()
     }
 
     private fun loadCategories() {
@@ -47,6 +50,40 @@ class DataLoader(
                 password = "password"
             )
             userRepository.save(user).also { logger.info(it.toString()) }
+        }
+    }
+    private fun loadArticles() {
+        if (articleRepository.count() == 0L) {
+            val categoryTechnology = categoryRepository.findAll().get(0)
+            val categoryWorld = categoryRepository.findAll().get(1)
+            val authors = authorRepository.saveAll(
+                listOf(
+                    Author(
+                        user = userRepository.findAll().get(0),
+                        about = "When an unknown printer took a galley of type and scrambled it to make a type specimen book."
+                    ),
+                    Author(
+                        user = userRepository.findAll().get(1),
+                        about = "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+                    )
+                )
+            )
+            listOf(
+                Article(
+                    title = "is simply dummy text of the printing and typesettin",
+                    subTitle = "industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+                    content = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
+                    date = LocalDateTime.now(),
+                    author = authors.get(0)
+                ),
+                Article(
+                    title = "There are many variations of passages of Lorem Ipsum available",
+                    subTitle = "or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything",
+                    content = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form.",
+                    date = LocalDateTime.now(),
+                    author = authors.get(1)
+                )
+            ).also { articleRepository.saveAll(it) }
         }
     }
 }
